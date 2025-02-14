d40d663ed85eea8900eeace2143d4f96a1773591
karllogdat <logdatk@gmail.com>
2025-02-14 13:40:25 +0800

    add shunting yard algo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ mod lexer;
 
 use lexer::{Lexer, Token};
 use std::io::{self, Write};
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+enum CalculatorError {
+    InvalidInput,
+    DivideByZero,
+    ParseError,
+}
 
 fn get_input() -> io::Result<String> {
     let mut input = String::new();
@@ -10,16 +19,59 @@ fn get_input() -> io::Result<String> {
     Ok(input.trim().to_string())
 }
 
-fn main() {
-    print!("Enter expression > ");
-    let input = get_input().unwrap();
+fn get_precedence(token: &Token) -> Option<u8> {
+    let op_precedence = HashMap::from([
+        ('+', 0),
+        ('-', 0),
+        ('*', 1),
+        ('/', 1),
+    ]);
+
+    match token {
+        Token::PLUS => Some(*op_precedence.get(&'+').unwrap()),
+        Token::MINUS => Some(*op_precedence.get(&'+').unwrap()),
+        Token::TIMES => Some(*op_precedence.get(&'+').unwrap()),
+        Token::DIVIDE => Some(*op_precedence.get(&'+').unwrap()),
+        _ => None,
+    }
+}
+
+fn get_postfix(input: &str) -> Result<Vec<Token>, CalculatorError> {
     let mut lexer = Lexer::new(&input);
+    let mut tokens: Vec<Token> = vec![];
+    let mut op_stack: VecDeque<Token> = VecDeque::new();
     loop {
         let token = lexer.next_token();
         match token {
+            Token::NUMBER(_) => tokens.push(token),
+            Token::PLUS | Token::MINUS | Token::TIMES | Token::DIVIDE => {
+                if op_stack.is_empty() {
+                    op_stack.push_front(token);
+                    continue
+                } else {
+                    while get_precedence(op_stack.front().unwrap()).unwrap() > get_precedence(&token).unwrap() {
+                        tokens.push(op_stack.pop_front().unwrap_or(Token::UNKNOWN));
+                    }
+                    op_stack.push_front(token);
+                }
+            }
             Token::EOF => break,
             Token::SPACE => continue,
-            _ => println!("{:?}", token),
+            _ => return Err(CalculatorError::ParseError),
         }
+    }
+
+    while !op_stack.is_empty() {
+        tokens.push(op_stack.pop_front().unwrap());
+    }
+
+    Ok(tokens)
+}
+
+fn main() {
+    print!("Enter expression > ");
+    let postfix = get_postfix(&get_input().unwrap()).unwrap();
+    for token in postfix {
+        println!("{:?}", token);
     }
 }
