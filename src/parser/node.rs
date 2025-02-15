e4e026532934ddd7daa0c110b2bd7c58fe2cd8f0
karllogdat <logdatk@gmail.com>
2025-02-15 19:00:44 +0800

    add node init unit tests

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -37,3 +37,53 @@ impl Node {
         }
     }
 }
+
+#[cfg(test)]
+mod node_tests {
+    use super::*;
+
+    #[test]
+    fn new_num_node() {
+        let num_tok = Node::Number(0.0);
+        let _actual = Node::new_num(Token::NUMBER(0.0)).unwrap();
+
+        matches!(num_tok, _actual);
+    }
+
+    #[test]
+    fn new_add_node() {
+        let add_tok = Node::Add(Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0)));
+        let _actual = Node::new_operator(&Token::PLUS, Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0))).unwrap();
+
+        matches!(add_tok, _actual);
+    }
+
+    #[test]
+    fn new_sub_node() {
+        let sub_tok = Node::Subtract(Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0)));
+        let _actual = Node::new_operator(&Token::MINUS, Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0))).unwrap();
+        matches!(sub_tok, _actual);
+    }
+
+    #[test]
+    fn new_mult_node() {
+        let tok = Node::Multiply(Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0)));
+        let _actual = Node::new_operator(&Token::TIMES, Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0))).unwrap();
+
+        matches!(tok, _actual);
+    }
+
+    #[test]
+    fn new_div_node() {
+        let tok = Node::Divide(Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0)));
+        let _actual = Node::new_operator(&Token::DIVIDE, Box::new(Node::Number(0.0)), Box::new(Node::Number(0.0))).unwrap();
+        matches!(tok, _actual);
+    }
+
+    #[test]
+    fn eval_num() {
+        if let Some(node) = Node::new_num(Token::NUMBER(727.0)) {
+            assert_eq!(727.0, node.evaluate());
+        }
+    }
+}
